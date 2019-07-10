a7d70b7f029a3a6bd84adccf707bb7fb0ada3e1b
Henrique Omena <88lmao@gmail.com>
2019-07-10 04:30:22 +0100

    Create modal and form install flatpicker and select2

--- resources/views/components/cargo/fieldCargo.blade.php
+++ resources/views/components/cargo/fieldCargo.blade.php
@@ -1,9 +1,14 @@
-<div class="row">
-    <select name="cargos">
-        @foreach ($cargos as $cargo)
-            <option value="{{ $cargo->id}}">
-                {{ $cargo->descricao }}
-            </option>
-        @endforeach
-    </select>
+<div class="row px-5">
+
+    <div class="row">
+        <select name="cargos" class="form-control">
+            @foreach ($cargos as $cargo)
+                <option value="{{ $cargo->id}}">
+                    {{ $cargo->descricao }}
+                </option>
+            @endforeach
+        </select>
+
+
+    </div>
 </div>
